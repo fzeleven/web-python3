605673b96b04ea76e33288c2f3ca50961c1e76c6
fzeleven <fzeleven>
2019-08-28 02:35:53 +0000

    generated code by contracts: bigpet.yaml

---
+++ openapi_server/__main__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import connexion
+
+from openapi_server import encoder
+
+
+def main():
+    app = connexion.App(__name__, specification_dir='./openapi/')
+    app.app.json_encoder = encoder.JSONEncoder
+    app.add_api('bigpet.yaml', arguments={'title': 'bigpet'}, pythonic_params=True)
+    # app.add_api('openapi.yaml', arguments={'title': 'openapi'}, pythonic_params=True)
+    app.run(port=8080)
+
+
+if __name__ == '__main__':
+    main()
